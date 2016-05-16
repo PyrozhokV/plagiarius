54c447770120ef7e80513e7ac20a0af5c76be212
PyrozhokHome <pyrozhok.vladyslav@gmail.com>
2016-05-16 23:52:10 +0300

    Stanford NLP was added

--- src/test/java/Sample.java
+++ src/test/java/Sample.java
@@ -3,6 +3,7 @@ import edu.smu.tspell.wordnet.Synset;
 import edu.smu.tspell.wordnet.SynsetType;
 import edu.smu.tspell.wordnet.WordNetDatabase;
 import org.apache.lucene.analysis.*;
+import org.apache.lucene.analysis.tokenattributes.TermAttribute;
 import org.apache.lucene.util.Version;
 import org.apache.lucene.wordnet.SynonymMap;
 import org.apache.lucene.wordnet.SynonymTokenFilter;
@@ -11,24 +12,14 @@ import org.testng.annotations.Test;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.Reader;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Pyrozhok on 10.05.2016.
  */
 public class Sample {
-
-    final String testFile = "C:\\Users\\Pyrozhok\\IdeaProjects\\pdffiller-tests_v2\\plagiarius\\src\\test\\resources\\synonyms.txt";
-
-    String[] words = new String[] { "hard", "woods", "forest", "wolfish", "xxxx"};
-
-    @Test
-    public void test() throws IOException {
-        SynonymMap map = new SynonymMap(new FileInputStream("samples/fulltext/wn_s.pl"));
-        for (int i = 0; i < words.length; i++) {
-            String[] synonyms = map.getSynonyms(words[i]);
-            System.out.println(words[i] + ":" + java.util.Arrays.asList(synonyms).toString());
-        }
-    }
 
     @Test
     public void test2() {
@@ -45,4 +36,60 @@ public class Sample {
                     ": " + nounSynset.getDefinition() + "; has " + hyponyms.length + " hyponyms");
         }
     }
+
+
+
+
+//    public class AnalyzedDBObject extends BasicDBObject {
+//        public static enum Condition {ALL,IN}
+//        private Analyzer analyzer;
+//
+//        public AnalyzedDBObject(Analyzer analyzer) {
+//            this.analyzer = analyzer;
+//        }
+//
+//        public AnalyzedDBObject appendAndAnalyzeFullText(String name, String text)
+//                throws IOException {
+//            append(name, tokenize(analyzer.tokenStream(name, new StringReader(text))));
+//            return this;
+//        }
+//
+//        private List<string> tokenize(TokenStream stream) throws IOException {
+//            List<string> tokens = new ArrayList<string>();
+//            TermAttribute term = (TermAttribute) stream
+//                    .addAttribute(TermAttribute.class);
+//            while (stream.incrementToken()) {
+//                // Not sure if we somehow can use termBuffer() to get a char[]
+//                // so we do no have to create a new String for each term
+//                tokens.add(term.term());
+//            }
+//            return tokens;
+//        }
+//    }
+//    public class AnalyzedDBObject extends BasicDBObject {
+//        public static enum Condition {ALL,IN}
+//        private Analyzer analyzer;
+//
+//        public AnalyzedDBObject(Analyzer analyzer) {
+//            this.analyzer = analyzer;
+//        }
+//
+////        public AnalyzedDBObject appendAndAnalyzeFullText(String name, String text)
+////                throws IOException {
+////            append(name, tokenize(analyzer.tokenStream(name, new StringReader(text))));
+////            return this;
+////        }
+//
+//        private List<String> tokenize(TokenStream stream) throws IOException {
+//            List<String> tokens = new ArrayList<String>();
+//            TermAttribute term = (TermAttribute) stream
+//                    .addAttribute(TermAttribute.class);
+//            while (stream.incrementToken()) {
+//                // Not sure if we somehow can use termBuffer() to get a char[]
+//                // so we do no have to create a new String for each term
+//                tokens.add(term.term());
+//            }
+//            return tokens;
+//        }
+//    }
 }
