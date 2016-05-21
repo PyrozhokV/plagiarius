1f29a080ed5b9669fd2002cf5ed969af96e4a2a8
Pyrozhok <pyrozhok.vladyslav@gmail.com>
2016-05-21 17:47:41 +0300

    Some samples

--- src/test/java/Sample.java
+++ src/test/java/Sample.java
@@ -2,6 +2,7 @@ import edu.smu.tspell.wordnet.NounSynset;
 import edu.smu.tspell.wordnet.Synset;
 import edu.smu.tspell.wordnet.SynsetType;
 import edu.smu.tspell.wordnet.WordNetDatabase;
+import edu.stanford.nlp.ling.Word;
 import org.apache.lucene.analysis.*;
 import org.apache.lucene.analysis.tokenattributes.TermAttribute;
 import org.apache.lucene.util.Version;
@@ -37,7 +38,39 @@ public class Sample {
         }
     }
 
+    @Test
+    public void test1() {
+        System.setProperty("wordnet.database.dir", "C:/jaws/wn3.1.dict/dict");
+        WordNetDatabase database = WordNetDatabase.getFileInstance();
+        List<Synset[]> synsets = new ArrayList<Synset[]>();
+        synsets.add(database.getSynsets("man"));
+        synsets.add(database.getSynsets("has"));
+        synsets.add(database.getSynsets("bought"));
+        synsets.add(database.getSynsets("new"));
+        synsets.add(database.getSynsets("red"));
+        synsets.add(database.getSynsets("car"));
 
+        System.out.println("ADJ = " + SynsetType.ADJECTIVE);
+        System.out.println("ADJ_SAT = " + SynsetType.ADJECTIVE_SATELLITE);
+        System.out.println("ADVERB = " + SynsetType.ADVERB);
+        System.out.println("NOUN = " + SynsetType.NOUN);
+        System.out.println("VERB = " + SynsetType.VERB);
+
+
+//        for (int i = 0; i < synsets.get(0)[0].getUsageExamples().length; i++) {
+//            System.out.println(synsets.get(0)[0].getUsageExamples()[i]);
+//        }
+        for (int i = 0; i < synsets.size(); i++) {
+            Synset[] current = synsets.get(i);
+            System.out.println("==================");
+            for (int j = 0; j < current.length; j++) {
+                String[] wordForms = current[j].getWordForms();
+                for (int k = 0; k < wordForms.length; k++) {
+                    System.out.println(wordForms[k]);
+                }
+            }
+        }
+    }
 
 
 //    public class AnalyzedDBObject extends BasicDBObject {
